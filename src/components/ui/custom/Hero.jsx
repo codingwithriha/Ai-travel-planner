cdfb54373439d19c3f1ba2c57d1da320951ba1d6
Codingwithriha <rihashehzadi2003@gmail.com>
2025-07-18 17:46:46 +0500

    impl: hero-section

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from '../button'
+import { Link } from 'react-router-dom'
 
 export default function Hero() {
   return (
@@ -10,7 +11,10 @@ export default function Hero() {
       <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.
 
 </p>
+<Link to={'/create-trip'}>
 <Button>Get Started, It's Free</Button>
+</Link>
+
     </div>
   )
 }
