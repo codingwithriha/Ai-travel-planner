aaa79877a8e052152c75741d1fd82134c13a72bf
Codingwithriha <rihashehzadi2003@gmail.com>
2025-07-24 19:07:06 +0500

    impl: footer-added

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -6,7 +6,7 @@ import { toast } from 'sonner';
 import InfoSection from '../components/InfoSection';
 import Hotels from '../components/Hotels';
 import PlacesToVisit from '../components/PlacesToVisit';
-// import Footer from '../components/Footer';
+import Footer from '../components/Footer';
 
 function Viewtrip() {
     const { tripId } = useParams();
@@ -45,7 +45,7 @@ function Viewtrip() {
             <PlacesToVisit trip={trip} />
 
             {/* Footer */}
-            {/* <Footer trip={trip} /> */}
+            <Footer trip={trip} />
 
         </div>
     )
